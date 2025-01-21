fd8022099898ecd03d00b1c08d752de05944f987
Anshul <anshulmendiratta10d@gmail.com>
2025-01-21 15:41:16 +0530

    ...

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,4 @@
 import { HeroSection } from "@/components/HomeBackground";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
