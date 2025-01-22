cbb94c85af627b64f3d01c9fc6380f72169eab55
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-01-22 12:46:19 +0530

    merge changes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,4 @@
 import { HeroSection } from "@/components/HomeBackground";
-<<<<<<< HEAD
-import { UserButton } from "@clerk/nextjs";
-import HomeSidebar from "@/components/HomeSidebar"
-import Image from "next/image";
-=======
->>>>>>> f42706717005fbe44d17ac85d300f1af45ffd520
 
 export default function Home() {
   return (
