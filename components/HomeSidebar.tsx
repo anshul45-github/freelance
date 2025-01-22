1173acec1e42b0543e7c7862230b6b459879d07e
Siddhant Shah <siddhantshah469@gmail.com>
2025-01-22 19:31:23 +0530

    updated HomeSidebar

--- components/HomeSidebar.tsx
+++ components/HomeSidebar.tsx
@@ -34,7 +34,7 @@ export function SidebarDemo() {
       ),
     },
     {
-      label: "Provide Talent",
+      label: "Upload Talent",
       href: "#",
       icon: (
         <IoBriefcaseOutline className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
