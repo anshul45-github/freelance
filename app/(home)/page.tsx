8eb4a966b2b7a1717e436caa586ef1a4b551abc2
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-01-22 23:28:42 +0530

    Final First Page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -62,10 +62,10 @@ export default function Home() {
             <CardBody className="relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1]  w-auto sm:w-[30rem] h-auto rounded-xl p-6">
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
-                  src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="/img1.jpg"
                   height="1000"
                   width="1000"
-                  className="h-52 md:h-full w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                  className="h-52 md:h-full w-full object-cover rounded-xl"
                   alt="thumbnail"
                 />
               </CardItem>
